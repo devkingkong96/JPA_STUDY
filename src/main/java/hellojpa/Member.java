cd1aa12c746393e74e71f12d1236d12a882aac40
ksj <kimsj9693@gmail.com>
2024-04-04 14:43:14 +0900

    일대일: 주 테이블에 외래 키 양방향 정리 • 다대일 양방향 매핑 처럼 외래 키가 있는 곳이 연관관계의 주인 • 반대편은 mappedBy 적용

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -18,6 +20,12 @@ public class Member {
     @ManyToOne
     @JoinColumn(name = "TEAM_ID",insertable = false, updatable = false) //읽기 전용이 된다.
     private Team team;
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
