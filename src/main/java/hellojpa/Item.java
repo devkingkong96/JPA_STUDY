e6adb340c5d579c4075e1fdf33bdb1785f2e709a
ksj <kimsj9693@gmail.com>
2024-04-12 11:17:14 +0900

    @MappedSuperclass • 테이블과 관계 없고, 단순히 엔티티가 공통으로 사용하는 매핑정보를 모으는 역할 • 주로 등록일, 수정일, 등록자, 수정자 같은 전체 엔티티에서 공통으로 적용하는 정보를 모을 때 사용

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,9 +3,10 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn //(name = "DIS_NAME")
 //DTYPE이란 게 생긴다(Item의 타입명) - Item만 select 했을 때 구분 가능
+//단일 테이블 전략에선 @DiscriminatorColumn을 안적어도 자동으로 입력됨.
 public class Item {
 
     @Id @GeneratedValue
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -16,19 +17,17 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Movie movie = new Movie();
-            movie.setDirector("김봉원");
-            movie.setActor("이소라");
-            movie.setName("바람과함께사라지다");
-            movie.setPrice(10000);
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("Kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            em.persist(movie);
+
+            em.persist(member);
 
             em.flush(); //영속성 컨텍스트의 변경 내용을 DB에 반영
             em.clear(); //영속성 컨텍스트 정리
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e){
