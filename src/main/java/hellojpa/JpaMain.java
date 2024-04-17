98fa442906e121f6e9ddea99f4122ea7a952ebe6
ksj <kimsj9693@gmail.com>
2024-04-17 19:10:37 +0900

    프록시 •영속성 detach, close, clear 를 하면 could not initialize proxy 예외가 뜬다!! 나중에 실무에서 만나면 기억!!

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -27,16 +27,25 @@ public class JpaMain {
 
 //            Member findMember = em.find(Member.class, member.getId());
 
-            Member m1 = em.find(Member.class, member1.getId());
-            System.out.println("m1 = " + m1.getClass());
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); //Proxy
 
-            Member reference = em.getReference(Member.class, member1.getId());
-            System.out.println("reference = " + reference.getClass());
+            em.detach(refMember); // 영속성 컨텍스트 refMember 관리 안한다.
+//            em.close(); //영속성 컨텍스트 종료
+
+            refMember.getUsername(); // 실제 DB에 쿼리가 나가면서 프록시가 조회가 된다.
+            System.out.println("refMember = " + refMember.getUsername());
+
+//          영속성 detach, close, clear 를 하면 could not initialize proxy 예외가 뜬다
+
+
+//            Member findMember = em.find(Member.class, member1.getId());
+//            System.out.println("findMember = " + findMember.getClass()); //Member? Proxy?
 
             // JPA에서 == 비교가 한 영속성 컨텍스트안에서 가져온 거고, pk가 같다면 true를 반환한다.
             // JPA는 한 트랜잭션 안에서 같은 거에 대해선 보장을 해준다.
             // 이미 영속성 컨텍스트안에 있으면 프록시가 아니라 실제 엔티티를 반환해준다.
-            System.out.println("a == a " + (m1 == reference));
+//            System.out.println("refMember == findMember : " + (refMember == findMember));
 //            System.out.println("findMember.id = " + findMember.getId());
 //            System.out.println("findMember.username = " + findMember.getUsername());
 
@@ -44,6 +53,7 @@ public class JpaMain {
             tx.commit();
         } catch (Exception e){
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
